55255436a482627991e65f6f499f714f35ef7331
Mikael Berglund <mikbe@kth.se>
2017-11-11 17:40:23 +0100

    prereqs

--- build-server.bat
+++ build-server.bat
@@ -4,4 +4,4 @@ set PARENT=%CD%
 popd
 
 echo %PARENT%
-%PARENT%\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="%PARENT%\GameLiftMultiplayer\GameLiftMultiplayer.uproject" -noP4 -serverconfig=Development -cook -iterate -server -serverplatform=win64 -noclient -build -stage -archive -archivedirectory="%PARENT%\Server"
+%PARENT%\UnrealEngine\Engine\Build\BatchFiles\RunUAT.bat BuildCookRun -project="%PARENT%\GameLiftMultiplayer\GameLiftMultiplayer.uproject" -noP4 -serverconfig=Development -prereqs -cook -iterate -server -serverplatform=win64 -noclient -build -stage -archive -archivedirectory="%PARENT%\Server"
